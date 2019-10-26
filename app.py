6141eda6442b7df32e49975cf8e3bee69aff8448
Abhijeet Thakurdesai <abhijeet01thakurdesai@gmail.com>
2019-10-26 00:17:37 -0700

    Added model and made changes to git ignore

--- app.py
+++ app.py
@@ -1,12 +1,25 @@
-from flask import Flask
+import datetime
+from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+class Food(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    date = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+    location = db.Column(db.String(500))
+    zipcode = db.Column(db.BIGINT())
 
+class FoodSchema(ma.ModelSchema):
+    class Meta:
+        model = Food
 
+db.create_all()
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
